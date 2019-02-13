f0d600884d4643946fc06a95d125386e695df465
huitian <tanhuitian@gmail.com>
2019-02-13 16:22:40 +0800

    Implement addtask function

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+// query selectors
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+
+// onload, create default list
+function myFunction() {
+  const todo = ["buy milk", "eat dinner", "nail javascript", "give feedback"];
+
+  todo.forEach(element => {
+    addtask(element);
+  });
+}
+
+function addtask(element) {
+  // create li w text
+  let li = document.createElement("li");
+  li.addEventListener("click", done(li));
+  let text = document.createTextNode(element);
+  li.appendChild(text);
+  const ol = document.querySelector("#todo-list");
+  ol.appendChild(li);
+  // create trash icon
+
+  // clear text from input field
+ 
+
+}
+
+submit.addEventListener("click", () => {
+  if (input.value !== "") {
+    addtask(input.value);
+  } else {
+    alert("Please input a task.");
+  }
+});
+
+function done(li) {
+  li.addEventListener("click", function() {
+    li.classList.toggle("done");
+  });
+}
